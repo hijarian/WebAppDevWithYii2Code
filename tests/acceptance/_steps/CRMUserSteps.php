4f7971898407785a16496d5dd2b3b3efc17dd619
Марк Сафронов <hijarian@gmail.com>
2014-07-04 17:55:22 +0400

    Added the acceptance test for our feature.

---
+++ tests/acceptance/_steps/CRMUserSteps.php
@@ -0,0 +1,37 @@
+<?php
+namespace AcceptanceTester;
+
+class CRMUserSteps extends \AcceptanceTester
+{
+    function amInQueryCustomerUi()
+    {
+        $I = $this;
+        $I->amOnPage('/customers/query');
+    }
+    function fillInPhoneFieldWithDataFrom($customer_data)
+    {
+        $I = $this;
+        $I->fillField('phone_number', $customer_data['PhoneRecord[number]']);
+    }
+    function clickSearchButton()
+    {
+        $I = $this;
+        $I->click('Search');
+    }
+    function seeIAmInListCustomersUi()
+    {
+        $I = $this;
+        $I->seeCurrentUrlMatches('/customers/');
+    }
+    function seeCustomerInList($customer_data)
+    {
+        $I = $this;
+        $I->see($customer_data['CustomerRecord[name]'], '#search_results');
+    }
+    function dontSeeCustomerInList($customer_data)
+    {
+        $I = $this;
+        $I->dontSee($customer_data['CustomerRecord[name]'], '#search_results');
+    }
+
+}
