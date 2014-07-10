d14803fcf8fd4f4fc180682a792b6f089ab1e33e
Марк Сафронов <hijarian@gmail.com>
2014-07-10 12:54:29 +0400

    Replaced the hand-crafted layout with the one expected by Gii.

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -2,13 +2,28 @@
 /**
  * @var string $content
  */
+use yii\helpers\Html;
+
+\yii\bootstrap\BootstrapAsset::register($this);
+\yii\web\YiiAsset::register($this);
 ?>
+<?php $this->beginPage() ?>
 <!DOCTYPE html>
-<html>
+<html lang="<?= Yii::$app->language ?>">
 <head>
-    <title>CRM</title>
+    <meta charset="<?= Yii::$app->charset ?>"/>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+    <?= Html::csrfMetaTags() ?>
 </head>
 <body>
-    <?= $content; ?>
+<?php $this->beginBody() ?>
+<div class="container">
+    <?= $content ?>
+    <footer class="footer"><?= Yii::powered();?></footer>
+</div>
+<?php $this->endBody() ?>
 </body>
 </html>
+<?php $this->endPage() ?>
+
