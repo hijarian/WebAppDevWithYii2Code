37f22e40f596fed9131f894009323c786239eae6
Марк Сафронов <hijarian@gmail.com>
2014-07-10 12:53:15 +0400

    Made the system depend on Gii also.

--- config/web.php
+++ config/web.php
@@ -2,11 +2,18 @@
 return [
     'id' => 'crmapp',
     'basePath' => realpath(__DIR__ . '/../'),
+    'modules' => [
+        'gii' => [
+            'class' => 'yii\gii\Module',
+            'allowedIPs' => ['*']
+        ]
+    ],
     'components' => [
         'db' => require(__DIR__ . '/db.php'),
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false
         ]
-    ]
+    ],
+    'extensions' => require(__DIR__ . '/../vendor/yiisoft/extensions.php')
 ];
--- web/index.php
+++ web/index.php
@@ -1,6 +1,8 @@
 <?php
 ini_set('display_errors', true);
 define('YII_DEBUG', true);
+// Including the Composer autoloader.
+require(__DIR__ . '/../vendor/autoload.php');
 // Including the Yii framework itself (1)
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 // Getting the configuration (2)
