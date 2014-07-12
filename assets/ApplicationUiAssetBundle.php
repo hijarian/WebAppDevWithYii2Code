81cc6b59f9bcc338aac4597c40db093f68822d53
Марк Сафронов <hijarian@gmail.com>
2014-07-12 21:56:38 +0400

    Added the Application UI Asset Bundle.

---
+++ assets/ApplicationUiAssetBundle.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hijarian
+ * Date: 12.07.14
+ * Time: 21:40
+ */
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+class ApplicationUiAssetBundle extends AssetBundle
+{
+    public $sourcePath = '@app/assets/ui';
+    public $css = [
+        'css/main.css'
+    ];
+    public $js = [
+        'js/main.js'
+    ];
+    public $depends = [
+        'yii\bootstrap\BootstrapAsset',
+        'yii\web\YiiAsset'
+    ];
+}
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -4,8 +4,7 @@
  */
 use yii\helpers\Html;
 
-\yii\bootstrap\BootstrapAsset::register($this);
-\yii\web\YiiAsset::register($this);
+app\assets\ApplicationUiAssetBundle::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
