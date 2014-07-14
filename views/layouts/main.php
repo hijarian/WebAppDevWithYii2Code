0d891e5c112c19150e5fae3f384638b851e5a3be
Марк Сафронов <hijarian@gmail.com>
2014-07-14 09:32:53 +0400

    Implemented the Authentication indicator.

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -18,6 +18,17 @@ app\assets\ApplicationUiAssetBundle::register($this);
 <body>
 <?php $this->beginBody() ?>
 <div class="container">
+
+    <div class="authorization-indicator">
+        <?php if (Yii::$app->user->isGuest):?>
+            <?= Html::tag('span', 'guest');?>
+            <?= Html::a('login', '/site/login');?>
+        <?php else:?>
+            <?= Html::tag('span', Yii::$app->user->identity->username);?>
+            <?= Html::a('logout', '/site/logout');?>
+        <?php endif;?>
+    </div>
+
     <?= $content ?>
     <footer class="footer"><?= Yii::powered();?></footer>
 </div>
