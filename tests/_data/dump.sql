40a3853663a13861015d91989c6c8ba02b1c2ed5
Марк Сафронов <hijarian@gmail.com>
2014-07-21 10:13:49 +0400

    Removed unnecessary lines from functional test suite bootstrap and regenerated the dump.

--- tests/_data/dump.sql
+++ tests/_data/dump.sql
@@ -32,6 +32,16 @@ CREATE TABLE `auth_assignment` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `auth_assignment`
+--
+
+LOCK TABLES `auth_assignment` WRITE;
+/*!40000 ALTER TABLE `auth_assignment` DISABLE KEYS */;
+INSERT INTO `auth_assignment` VALUES ('admin','3',1405923177),('manager','2',1405923177),('user','1',1405923177);
+/*!40000 ALTER TABLE `auth_assignment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `auth_item`
 --
 
@@ -54,6 +64,16 @@ CREATE TABLE `auth_item` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `auth_item`
+--
+
+LOCK TABLES `auth_item` WRITE;
+/*!40000 ALTER TABLE `auth_item` DISABLE KEYS */;
+INSERT INTO `auth_item` VALUES ('admin',1,'Can do anything including managing users',NULL,NULL,1405923177,1405923177),('guest',1,'Nobody',NULL,NULL,1405923177,1405923177),('manager',1,'Can manage entities in database, but not users',NULL,NULL,1405923177,1405923177),('user',1,'Can use the query UI and nothing else',NULL,NULL,1405923177,1405923177);
+/*!40000 ALTER TABLE `auth_item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `auth_item_child`
 --
 
@@ -69,6 +89,16 @@ CREATE TABLE `auth_item_child` (
   CONSTRAINT `auth_item_child_ibfk_2` FOREIGN KEY (`child`) REFERENCES `auth_item` (`name`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `auth_item_child`
+--
+
+LOCK TABLES `auth_item_child` WRITE;
+/*!40000 ALTER TABLE `auth_item_child` DISABLE KEYS */;
+INSERT INTO `auth_item_child` VALUES ('user','guest'),('admin','manager'),('manager','user');
+/*!40000 ALTER TABLE `auth_item_child` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `auth_rule`
@@ -87,6 +117,15 @@ CREATE TABLE `auth_rule` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `auth_rule`
+--
+
+LOCK TABLES `auth_rule` WRITE;
+/*!40000 ALTER TABLE `auth_rule` DISABLE KEYS */;
+/*!40000 ALTER TABLE `auth_rule` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `customer`
 --
 
@@ -103,6 +142,15 @@ CREATE TABLE `customer` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `customer`
+--
+
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `migration`
 --
 
@@ -115,6 +163,16 @@ CREATE TABLE `migration` (
   PRIMARY KEY (`version`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `migration`
+--
+
+LOCK TABLES `migration` WRITE;
+/*!40000 ALTER TABLE `migration` DISABLE KEYS */;
+INSERT INTO `migration` VALUES ('m000000_000000_base',1405923175),('m140506_102106_rbac_init',1405923176),('m140704_074257_init_customer_table',1405923176),('m140704_074646_init_phones_table',1405923176),('m140710_083424_init_services_table',1405923176),('m140713_053357_create_user_table',1405923176),('m140714_050445_add_auth_key_to_user',1405923176),('m140718_063423_add_predefined_users',1405923177),('m140719_083354_create_roles_for_predefined_users',1405923177);
+/*!40000 ALTER TABLE `migration` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `phone`
@@ -134,6 +192,15 @@ CREATE TABLE `phone` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `phone`
+--
+
+LOCK TABLES `phone` WRITE;
+/*!40000 ALTER TABLE `phone` DISABLE KEYS */;
+/*!40000 ALTER TABLE `phone` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `service`
 --
 
@@ -148,6 +215,15 @@ CREATE TABLE `service` (
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `service`
+--
+
+LOCK TABLES `service` WRITE;
+/*!40000 ALTER TABLE `service` DISABLE KEYS */;
+/*!40000 ALTER TABLE `service` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `user`
@@ -166,6 +242,16 @@ CREATE TABLE `user` (
   UNIQUE KEY `auth_key` (`auth_key`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'JoeUser','$2y$13$QuVwSCjeMWQ9K2wwm5UpZOucjL4lDJcxNjBJVncBTAM3DpH..6XR.','33UgKyBr4Et3RK9D-UEl8MgHru6E9Tcbm2x6xTZ7eucgNVTYXLRBG4uXwDs7Q5Gt6QWb3lX0taXhBP0Lo7oYwf09--iYkYX2hKZAZRmGSM7kp5ufn0DHcHVy4dA3zzCr0byVSUcqPGGt_x2A1P5C5kVmmJzJlZgW29U4_UNCFnFR8kPkVQVxnK9uWl8fJ_CzbVkoct__gQMsSNrAdU0X3wAC_jFXMRAklB-WQIUMUhjBBqHepb_QTXKB0KD2BOT'),(2,'AnnieManager','$2y$13$ujaf3TP5gRSOgwPiLekEU.htAbXsVHYYYtXaIYQ/dAMsekEONtRaa','iGhHH_d1DbYAk6HoRFWWAzKIBXTGllM8geI0tNIZaW7Vir6L93PIcUDRoVA7bJViI0IjtbyLAACFKKfiVXu-KqMWawNW2ftKsGm9347ktkX4Yd912MeEOY1J_Fft1LQI5oHJD2W4d3AFdaHCzbGVn_5LuInfDX3b2cR72f1wlekIGKvkO2-pqP1X-cRij1olxQoX90r01dvfTG35x498Nj8TfpjLI9A0LQ80KUNm4Y0QogIW5O06_8Io4xqLSTd'),(3,'RobAdmin','$2y$13$gVGjzxoLfA/92MIiDCxQxODCu0HmENJmQM/NrOagA4JN3aNaihk6q','Rvp-05yZx4EH4TgEOXqWEdljaoHPl2rAevd8ny6oNe9C-npnS3_EkFC2NEwFmZYCFPt2DaQXsPYK3pYwYTVdJ9vd-nm7ORHytb6tMUEI0yss7i2vvJ-Qvv7aOkaTBcAnK467CszY47z5v4rc8q58ohWZupU08Z7fmlvRPI2kNT6dfspG2Lis-oFHtPUSdl3Iu96z4W7RE6b-q-c7z99MMl6lKVloIMf82aOWSrGCUEh5SO_sl5CF-v_upJbwhYV');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -176,4 +262,4 @@ CREATE TABLE `user` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-07-21  6:01:02
+-- Dump completed on 2014-07-21  6:12:57
--- tests/functional/_bootstrap.php
+++ tests/functional/_bootstrap.php
@@ -2,9 +2,6 @@
 require_once(__DIR__ . '/../../vendor/autoload.php');
 require_once(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
 
-passthru('mysql -u root -pmysqlroot -e "drop database if exists crmapp_test; create database crmapp_test default character set utf8 default collate utf8_unicode_ci"');
-passthru('mysqldump -u root -pmysqlroot -d crmapp > tests/_data/dump.sql');
-
 new yii\web\Application(
     require(__DIR__ . '/../../config/test.php')
 );
