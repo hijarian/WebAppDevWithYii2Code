7c9f7a9ae1324b2a4607b879474cbc9d84862da1
Марк Сафронов <hijarian@gmail.com>
2014-07-26 13:42:58 +0400

    Made the boilerplate for the asset bundle holding minified and compressed CSS and JavaScript files.

---
+++ assets/AllAsset.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hijarian
+ * Date: 26.07.14
+ * Time: 12:58
+ */
+
+namespace app\assets;
+use yii\web\AssetBundle;
+class AllAsset extends AssetBundle { }
