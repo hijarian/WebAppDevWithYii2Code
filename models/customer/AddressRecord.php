50faf240b5ae2fa666904792861f298d1f33bc6c
Марк Сафронов <hijarian@gmail.com>
2014-08-03 09:55:28 +0400

    Generated the model classes for `address` and `email` tables by Gii.

---
+++ models/customer/AddressRecord.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace app\models\customer;
+
+use Yii;
+
+/**
+ * This is the model class for table "address".
+ *
+ * @property integer $id
+ * @property string $purpose
+ * @property string $country
+ * @property string $state
+ * @property string $city
+ * @property string $street
+ * @property string $building
+ * @property string $apartment
+ * @property string $receiver_name
+ * @property string $postal_code
+ * @property integer $customer_id
+ *
+ * @property Customer $customer
+ */
+class AddressRecord extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'address';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['customer_id'], 'required'],
+            [['customer_id'], 'integer'],
+            [['purpose', 'country', 'state', 'city', 'street', 'building', 'apartment', 'receiver_name', 'postal_code'], 'string', 'max' => 255]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'purpose' => 'Purpose',
+            'country' => 'Country',
+            'state' => 'State',
+            'city' => 'City',
+            'street' => 'Street',
+            'building' => 'Building',
+            'apartment' => 'Apartment',
+            'receiver_name' => 'Receiver Name',
+            'postal_code' => 'Postal Code',
+            'customer_id' => 'Customer ID',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCustomer()
+    {
+        return $this->hasOne(Customer::className(), ['id' => 'customer_id']);
+    }
+}
---
+++ models/customer/EmailRecord.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace app\models\customer;
+
+use Yii;
+
+/**
+ * This is the model class for table "email".
+ *
+ * @property integer $id
+ * @property string $purpose
+ * @property string $address
+ * @property integer $customer_id
+ *
+ * @property Customer $customer
+ */
+class EmailRecord extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'email';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['customer_id'], 'required'],
+            [['customer_id'], 'integer'],
+            [['purpose', 'address'], 'string', 'max' => 255]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'purpose' => 'Purpose',
+            'address' => 'Address',
+            'customer_id' => 'Customer ID',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCustomer()
+    {
+        return $this->hasOne(Customer::className(), ['id' => 'customer_id']);
+    }
+}
